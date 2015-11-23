5f2f9eb73ab4d49274629aecb642996adfa8c5f8
Greg Baker <greg.baker.bsc@gmail.com>
2015-11-22 23:27:14 -0330

    fixed tooltips added application icon

--- src/main/java/com/github/xxbeanxx/noticeseditor/NoticesEditor.java
+++ src/main/java/com/github/xxbeanxx/noticeseditor/NoticesEditor.java
@@ -1,9 +1,12 @@
 package com.github.xxbeanxx.noticeseditor;
 
+import java.io.InputStream;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +19,9 @@ public class NoticesEditor extends Application {
     final Parent root = FXMLLoader
         .load(getClass().getResource("/com/github/xxbeanxx/noticeseditor/FXMLDocument.fxml"));
     final Scene scene = new Scene(root);
+    final InputStream inputStream = getClass().getResourceAsStream("/application-xml.png");
+    final Image image = new Image(inputStream);
+    stage.getIcons().add(image);
     stage.setTitle("MSCA Notices Editor");
     stage.setScene(scene);
     stage.show();
--- src/main/java/com/github/xxbeanxx/noticeseditor/controls/NoticeListCell.java
+++ src/main/java/com/github/xxbeanxx/noticeseditor/controls/NoticeListCell.java
@@ -1,27 +1,23 @@
 package com.github.xxbeanxx.noticeseditor.controls;
 
 import java.text.DateFormat;
-import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 import javax.xml.datatype.XMLGregorianCalendar;
 
+import org.jsoup.Jsoup;
+
 import com.github.xxbeanxx.noticeseditor.bindings.Notices.Notice;
 
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.Tooltip;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 
 /**
  * @author Greg Baker
  */
 public class NoticeListCell extends ListCell<Notice> {
-
-  private static final String TOOLTIP_PATTERN = "<h1>{0}</h1><strong>{1}</strong><div>{2}</div>";
 
   private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -36,27 +32,31 @@ public class NoticeListCell extends ListCell<Notice> {
       final String text = this.dateFormat.format(time);
       super.setText(text);
 
-      final String englishTitle = notice.getTitle().getEnglish();
-      final String englishText = notice.getText().getEnglish();
-
-      final String frenchTitle = notice.getTitle().getFrench();
-      final String frenchText = notice.getText().getFrench();
-
-      final WebView webView = new WebView();
-      final WebEngine webEngine = webView.getEngine();
-
-
-      webEngine.loadContent(
-          MessageFormat.format(NoticeListCell.TOOLTIP_PATTERN, "English", englishTitle, englishText)
-              + "<hr/>" + MessageFormat.format(NoticeListCell.TOOLTIP_PATTERN, "French",
-                  frenchTitle, frenchText));
-
-      final Tooltip tooltip = new Tooltip();
-      tooltip.setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
-      tooltip.setGraphic(webView);
-
+      final Tooltip tooltip = getTooltip(notice);
       super.setTooltip(tooltip);
     }
+  }
+
+  private Tooltip getTooltip(Notice notice) {
+    final StringBuilder stringBuilder = new StringBuilder();
+
+    final String englishTitle = notice.getTitle().getEnglish();
+    final String englishText = notice.getText().getEnglish();
+
+    final String frenchTitle = notice.getTitle().getFrench();
+    final String frenchText = notice.getText().getFrench();
+
+    // @formatter:off
+    stringBuilder.append("English:").append(System.lineSeparator());
+    stringBuilder.append("Title: ").append(Jsoup.parse(englishTitle).text()).append(System.lineSeparator());
+    stringBuilder.append(Jsoup.parse(englishText).body().children()).append(System.lineSeparator());
+    stringBuilder.append("--------------------------------").append(System.lineSeparator());
+    stringBuilder.append("French:").append(System.lineSeparator());
+    stringBuilder.append("Title: ").append(Jsoup.parse(frenchTitle).text()).append(System.lineSeparator());
+    stringBuilder.append(Jsoup.parse(frenchText).body().children()).append(System.lineSeparator());
+    // @formatter:on
+    
+    return new Tooltip(stringBuilder.toString());
   }
 
 }
